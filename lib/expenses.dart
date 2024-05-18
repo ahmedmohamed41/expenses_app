ea4f146b8708ec21f44cdda7fe2f8ca0b8418ca9
ahmed khodary <ahmedmohamed2019.com@gmail.com>
2024-05-19 02:24:08 +0300

    create new app

---
+++ lib/expenses.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatelessWidget {
+  const Expenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('....'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('...'),
+            Text('...'),
+          ],
+        ),
+      ),
+    );
+  }
+}
